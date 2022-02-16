8a9e4afdc97acf1b8de117fac6ddddc584d60a6a
guillaume levasseur <levasseurguillaume91@gmail.com>
2022-02-16 17:40:17 +0100

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'test 1',
+  address: 'test 1',
+  description: 'test 1',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'test 2',
+  address: 'test 2',
+  description: 'test 2',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'test 3',
+  address: 'test 3',
+  description: 'test 3',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'test 4',
+  address: 'test 4',
+  description: 'test 4',
+  price_per_night: 75,
+  number_of_guests: 3
+)
